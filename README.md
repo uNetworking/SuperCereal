fb14ef201b077af94990bc9c3038f143877719c8
Alex Hultman <alexhultman@localhost.localdomain>
2017-07-23 14:23:18 +0200

    More work on high level interface, better benchmarks, updated sample

--- README.md
+++ README.md
@@ -4,43 +4,54 @@
 **SuperCereal** is a simple and efficient JSON serialization library for Go. Unlike most other serializers it doesn't operate using an intermediary tree data structure (think "DOM") but instead exposes a set of simple fuction calls to directly control the JSON generation (a la "SAX"). This makes for a very lightweight and efficient JSON serialization process where most execution paths incur no memory allocations.
 
 ### Simple
-With an object oriented & generic design it takes only a few lines to generate JSON:
+With an object oriented & generic design it only takes a few lines to generate JSON:
 ```go
-json := supercereal.Serialize(func(object supercereal.JSONObject) {
+// Allocates memory, keep it alive and reuse it for many serializations!
+js := supercereal.NewJSONStream()
+
+// Register a callback to receive json output in (potentially) chunks of bytes.
+js.OnJSON(func(json []byte) {
+	fmt.Println(string(json))
+})
+
+// Perform the actual serialization of your data.
+js.Serialize(func(object *supercereal.JSONObject) {
 	object.Put("firstName", "John")
 	object.Put("lastName", "Smith")
 	object.Put("isAlive", true)
 	object.Put("age", 25)
 
-	object.Put("address", func(object supercereal.JSONObject) {
+	object.Put("address", func(object *supercereal.JSONObject) {
 		object.Put("streetAddress", "21 2nd Street")
 		object.Put("city", "New York")
 		object.Put("state", "NY")
 		object.Put("postalCode", "10021-3100")
 	})
 
-	object.Put("phoneNumbers", func(array supercereal.JSONArray) {
-		array.Put(func(object supercereal.JSONObject) {
+	object.Put("phoneNumbers", func(array *supercereal.JSONArray) {
+		array.Put(func(object *supercereal.JSONObject) {
 			object.Put("type", "home")
 			object.Put("number", "212 555-1234")
 		})
-		array.Put(func(object supercereal.JSONObject) {
+		array.Put(func(object *supercereal.JSONObject) {
 			object.Put("type", "office")
 			object.Put("number", "646 555-4567")
 		})
-		array.Put(func(object supercereal.JSONObject) {
+		array.Put(func(object *supercereal.JSONObject) {
 			object.Put("type", "mobile")
 			object.Put("number", "123 456-7890")
 		})
 	})
 
-	object.Put("children", func(array supercereal.JSONArray) {
+	object.Put("children", func(array *supercereal.JSONArray) {
 	})
 
 	object.Put("spouse", nil)
 })
-
 ```
+
+Above sample prints: `{"firstName":"John","lastName":"Smith","isAlive":true,"age":25,"address":{"streetAddress":"21 2nd Street","city":"New York","state":"NY","postalCode":"10021-3100"},"phoneNumbers":[{"type":"home","number":"212 555-1234"},{"type":"office","number":"646 555-4567"},{"type":"mobile","number":"123 456-7890"}],"children":[],"spouse":null}`
+
 
 ### Efficient
 The Go standard library call `json.Marshal` expects an `interface{}` which is to represent the root of an intermediary tree data structure. This means you need to allocate nodes, populate these nodes and finally traverse them all in order to generate JSON output. This my friend, this is madness.
--- main.go
+++ main.go
@@ -8,142 +8,140 @@ import (
 	"github.com/alexhultman/SuperCereal/supercereal"
 )
 
-func benchmarkSuperCereal() {
-	js := supercereal.NewJSONStream(func(data []byte) {
-		// this is where you get data (potentially in chunks)
-		// it is up to you how you send / store / print / buffer this
-		fmt.Print(string(data))
+func benchmarkSuperCerealHighLevel() {
+	var lastJSON []byte
+	js := supercereal.NewJSONStream()
+	js.OnJSON(func(json []byte) {
+		lastJSON = json
 	})
 
 	start := time.Now()
 	for i := 0; i < 1000000; i++ {
-		js.Reset()
+		js.Serialize(func(object *supercereal.JSONObject) {
+			object.Put("firstName", "John")
+			object.Put("lastName", "Smith")
+			object.Put("isAlive", true)
+			object.Put("age", 25)
 
-		// "stream" JSON data in (example from Wikipedia)
-		// todo: improve interfaces (helpers, etc, open and close pairs can be actual functions)
-		js.PutKey([]byte("firstName"))
-		js.PutString([]byte("John"))
+			object.Put("address", func(object *supercereal.JSONObject) {
+				object.Put("streetAddress", "21 2nd Street")
+				object.Put("city", "New York")
+				object.Put("state", "NY")
+				object.Put("postalCode", "10021-3100")
+			})
 
-		js.PutKey([]byte("lastName"))
-		js.PutString([]byte("Smith"))
+			object.Put("phoneNumbers", func(array *supercereal.JSONArray) {
+				array.Put(func(object *supercereal.JSONObject) {
+					object.Put("type", "home")
+					object.Put("number", "212 555-1234")
+				})
+				array.Put(func(object *supercereal.JSONObject) {
+					object.Put("type", "office")
+					object.Put("number", "646 555-4567")
+				})
+				array.Put(func(object *supercereal.JSONObject) {
+					object.Put("type", "mobile")
+					object.Put("number", "123 456-7890")
+				})
+			})
 
-		js.PutKey([]byte("isAlive"))
-		js.PutBoolean(true)
+			object.Put("children", func(array *supercereal.JSONArray) {
+			})
 
-		js.PutKey([]byte("age"))
-		js.PutInt(25)
-
-		js.PutKey([]byte("address"))
-		js.OpenObject()
-		{
-			js.PutKey([]byte("streetAddress"))
-			js.PutString([]byte("21 2nd Street"))
-
-			js.PutKey([]byte("city"))
-			js.PutString([]byte("New York"))
-
-			js.PutKey([]byte("state"))
-			js.PutString([]byte("NY"))
-
-			js.PutKey([]byte("postalCode"))
-			js.PutString([]byte("10021-3100"))
-		}
-		js.CloseObject()
-
-		js.PutKey([]byte("phoneNumbers"))
-		js.OpenArray()
-		{
-			js.OpenObject()
-			{
-				js.PutKey([]byte("type"))
-				js.PutString([]byte("home"))
-
-				js.PutKey([]byte("number"))
-				js.PutString([]byte("212 555-1234"))
-			}
-			js.CloseObject()
-
-			js.OpenObject()
-			{
-				js.PutKey([]byte("type"))
-				js.PutString([]byte("office"))
-
-				js.PutKey([]byte("number"))
-				js.PutString([]byte("646 555-4567"))
-			}
-			js.CloseObject()
-
-			js.OpenObject()
-			{
-				js.PutKey([]byte("type"))
-				js.PutString([]byte("mobile"))
-
-				js.PutKey([]byte("number"))
-				js.PutString([]byte("123 456-7890"))
-			}
-			js.CloseObject()
-		}
-		js.CloseArray()
-
-		js.PutKey([]byte("children"))
-		js.OpenArray()
-		js.CloseArray()
-
-		js.PutKey([]byte("spouse"))
-		js.PutNull()
+			object.Put("spouse", nil)
+		})
 	}
 
-	fmt.Printf("\n\nSerialization took %f µs\n", time.Since(start).Seconds())
-	js.End()
+	fmt.Printf("benchmarkSuperCerealHighLevel took %f µs\n", time.Since(start).Seconds())
+	fmt.Printf("%s\n\n", string(lastJSON))
 }
 
-func benchmarkSuperCerealSimple() {
-	js := supercereal.NewJSONStream(func(data []byte) {
-		fmt.Print(string(data))
+func benchmarkSuperCerealLowLevel() {
+	js := supercereal.NewJSONStream()
+	js.OnJSON(func(json []byte) {
+		fmt.Printf("%s\n\n", string(json))
 	})
 
 	start := time.Now()
 	for i := 0; i < 1000000; i++ {
-
-		// reset kan vara den enda funktionen i rooten
-		// som ger dig antingen en array eller ett object!
-		// då tas även End bort!
 		js.Reset()
+		js.OpenObject()
+		{
+			js.PutKey([]byte("firstName"))
+			js.PutString([]byte("John"))
 
-		js.Put("firstName", "John")
-		js.Put("lastName", "Smith")
-		js.Put("isAlive", true)
-		js.Put("age", 25)
+			js.PutKey([]byte("lastName"))
+			js.PutString([]byte("Smith"))
 
-		js.Put("address", func(object supercereal.JSONObject) {
-			object.Put("streetAddress", "21 2nd Street")
-			object.Put("city", "New York")
-			object.Put("state", "NY")
-			object.Put("postalCode", "10021-3100")
-		})
+			js.PutKey([]byte("isAlive"))
+			js.PutBoolean(true)
 
-		js.Put("phoneNumbers", func(array supercereal.JSONArray) {
-			array.Put(func(object supercereal.JSONObject) {
-				object.Put("type", "home")
-				object.Put("number", "212 555-1234")
-			})
-			array.Put(func(object supercereal.JSONObject) {
-				object.Put("type", "office")
-				object.Put("number", "646 555-4567")
-			})
-			array.Put(func(object supercereal.JSONObject) {
-				object.Put("type", "mobile")
-				object.Put("number", "123 456-7890")
-			})
-		})
+			js.PutKey([]byte("age"))
+			js.PutInt(25)
 
-		js.Put("children", func(array supercereal.JSONArray) {
-		})
+			js.PutKey([]byte("address"))
+			js.OpenObject()
+			{
+				js.PutKey([]byte("streetAddress"))
+				js.PutString([]byte("21 2nd Street"))
 
-		js.Put("spouse", nil)
+				js.PutKey([]byte("city"))
+				js.PutString([]byte("New York"))
+
+				js.PutKey([]byte("state"))
+				js.PutString([]byte("NY"))
+
+				js.PutKey([]byte("postalCode"))
+				js.PutString([]byte("10021-3100"))
+			}
+			js.CloseObject()
+
+			js.PutKey([]byte("phoneNumbers"))
+			js.OpenArray()
+			{
+				js.OpenObject()
+				{
+					js.PutKey([]byte("type"))
+					js.PutString([]byte("home"))
+
+					js.PutKey([]byte("number"))
+					js.PutString([]byte("212 555-1234"))
+				}
+				js.CloseObject()
+
+				js.OpenObject()
+				{
+					js.PutKey([]byte("type"))
+					js.PutString([]byte("office"))
+
+					js.PutKey([]byte("number"))
+					js.PutString([]byte("646 555-4567"))
+				}
+				js.CloseObject()
+
+				js.OpenObject()
+				{
+					js.PutKey([]byte("type"))
+					js.PutString([]byte("mobile"))
+
+					js.PutKey([]byte("number"))
+					js.PutString([]byte("123 456-7890"))
+				}
+				js.CloseObject()
+			}
+			js.CloseArray()
+
+			js.PutKey([]byte("children"))
+			js.OpenArray()
+			js.CloseArray()
+
+			js.PutKey([]byte("spouse"))
+			js.PutNull()
+		}
+		js.CloseObject()
 	}
 
-	fmt.Printf("Serialization took %f µs\n", time.Since(start).Seconds())
+	fmt.Printf("benchmarkSuperCerealLowLevel took %f µs\n", time.Since(start).Seconds())
 	js.End()
 }
 
@@ -185,12 +183,14 @@ func benchmarkJSONMarshal() {
 		bytes, _ = json.Marshal(root)
 	}
 
-	fmt.Printf("\n\nSerialization took %f µs\n", time.Since(start).Seconds())
-	fmt.Println(string(bytes))
+	fmt.Printf("benchmarkJSONMarshal took %f µs\n", time.Since(start).Seconds())
+	fmt.Printf("%s\n\n", string(bytes))
 }
 
 func main() {
-	benchmarkSuperCerealSimple() // high level interface
-	benchmarkSuperCereal()       // low level interface
-	//benchmarkJSONMarshal()
+	for i := 0; i < 5; i++ {
+		benchmarkSuperCerealHighLevel()
+		benchmarkSuperCerealLowLevel()
+	}
+	benchmarkJSONMarshal()
 }
