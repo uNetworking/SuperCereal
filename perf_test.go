d0ed4babb9be2f41c2cbb4be72d0525bd1f25233
Alex Hultman <alexhultman@gmail.com>
2019-04-25 01:42:18 +0200

    Add perf_test.go in Go style

---
+++ perf_test.go
@@ -0,0 +1,80 @@
+/* Run benchmarks with go test -bench . */
+
+package main
+
+import (
+	"testing"
+	"encoding/json"
+	"./supercereal"
+)
+
+func BenchmarkSuperCereal(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		supercereal.Marshal(func(object *supercereal.JSONObject) {
+			object.Put("firstName", "John")
+			object.Put("lastName", "Smith")
+			object.Put("isAlive", true)
+			object.Put("age", 25)
+
+			object.Put("address", func(object *supercereal.JSONObject) {
+				object.Put("streetAddress", "21 2nd Street")
+				object.Put("city", "New York")
+				object.Put("state", "NY")
+				object.Put("postalCode", "10021-3100")
+			})
+
+			object.Put("phoneNumbers", func(array *supercereal.JSONArray) {
+				array.Put(func(object *supercereal.JSONObject) {
+					object.Put("type", "home")
+					object.Put("number", "212 555-1234")
+				})
+				array.Put(func(object *supercereal.JSONObject) {
+					object.Put("type", "office")
+					object.Put("number", "646 555-4567")
+				})
+				array.Put(func(object *supercereal.JSONObject) {
+					object.Put("type", "mobile")
+					object.Put("number", "123 456-7890")
+				})
+			})
+
+			object.Put("children", func(array *supercereal.JSONArray) {
+			})
+
+			object.Put("spouse", nil)
+		})
+	}
+}
+
+func BenchmarkJSONMarshal(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		json.Marshal(map[string]interface{}{
+			"firstName": "John",
+			"lastName":  "Smith",
+			"isAlive":   true,
+			"age":       25,
+			"address": map[string]interface{}{
+				"streetAddress": "21 2nd Street",
+				"city":          "New York",
+				"state":         "NY",
+				"postalCode":    "10021-3100",
+			},
+			"phoneNumbers": []interface{}{
+				map[string]interface{}{
+					"type":   "home",
+					"number": "212 555-1234",
+				},
+				map[string]interface{}{
+					"type":   "office",
+					"number": "646 555-4567",
+				},
+				map[string]interface{}{
+					"type":   "mobile",
+					"number": "123 456-7890",
+				},
+			},
+			"children": []interface{}{},
+			"spouse":   nil,
+		})
+	}
+}
